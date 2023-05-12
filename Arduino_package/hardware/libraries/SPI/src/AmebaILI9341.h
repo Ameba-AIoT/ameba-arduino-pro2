45ccbf8b5292f6aa6b911a734c6c8d70e6e193c7
Dakamaster <644165318.tj@gmail.com>
2023-05-12 11:25:21 +0800

    Add SPI API drawBitmap() (#94)
    * Add SPI API drawBitmap()
    - modify minor typo in Serial.println()
    
    * Update for coding style

--- Arduino_package/hardware/libraries/SPI/src/AmebaILI9341.h
+++ Arduino_package/hardware/libraries/SPI/src/AmebaILI9341.h
@@ -3,6 +3,7 @@
 
 #include "Print.h"
 #include <cstdint>
+#include "SPI.h"
 
 #define ILI9341_TFTWIDTH     240
 #define ILI9341_TFTHEIGHT    320
@@ -90,7 +91,6 @@
 #define ILI9341_PINK        0xF81F
 
 class AmebaILI9341 : public Print {
-
 public:
     AmebaILI9341(int csPin, int dcPin, int resetPin);
 
@@ -108,7 +108,7 @@ public:
     void clr();
 
     void fillRectangle(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
-
+    void drawBitmap(int16_t x, int16_t y, int16_t w, int16_t h, const unsigned short *color);
     void drawPixel(int16_t x, int16_t y, uint16_t color);
 
     void drawChar(int16_t x, int16_t y, unsigned char c, uint16_t fontcolor, uint16_t background, uint8_t fontsize);
