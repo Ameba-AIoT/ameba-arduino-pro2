7354b245de50d7360a58a6f59e47d1ee749b8c87
Zhu Qi <zhuqi@realtek-sg.com>
2023-07-13 16:02:37 +0800

    Fix minor bug issue 1. Fix prebuild_tool.c to remove redundant parameter check due to number of parameter variation during compilation, rebuild prebuild_linux and prebuild_macos 2. Update mm_model_update_linux for robustness support
    Verification:
    macOS Intel/M2 Silicon Arduino IDE1&2 with Empty Sketch and ExamleDebug.ino

--- Ameba_misc/Ameba_tools/prebuild_tool/version_c/prebuild_tool/prebuild_tool.c
+++ Ameba_misc/Ameba_tools/prebuild_tool/version_c/prebuild_tool/prebuild_tool.c
@@ -51,14 +51,6 @@ void deleteDirectory(const char* path);
 void removeLastPathComponent(char* path);
 
 int main(int argc, char *argv[]) { 
-	// Check if the number of input arguments is correct 
-	if (argc != 5) { 
-#if PRINT_DEBUG
-		printf("[Error] Incorrect number of input parameters. Expected 4 parameters.\r\n"); 
-#endif
-		exit(1);
-	} 
-
 	// Retrieve the input parameters 
 	char* toolchain_p1_path = argv[1];
 	char* toolchain_p2_path = argv[2];
@@ -212,8 +204,8 @@ void deleteDirectory(const char* path) {
 }
 
 void removeLastPathComponent(char* path) {
-	int length = strlen(path);
-	int i;
+	size_t length = strlen(path);
+	size_t i;
 
 	for (i = length - 1; i >= 0; i--) {
 		if (path[i] == PATH_SEPARATOR) {
