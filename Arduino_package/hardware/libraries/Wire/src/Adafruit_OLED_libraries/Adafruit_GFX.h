cf90b2cb16cfaf09320ac91b3e6e255b7d6a1f45
pammyleong <pammyleong96@gmail.com>
2024-02-14 17:28:05 +0800

    Remove not necessary libs

--- Arduino_package/hardware/libraries/Wire/src/Adafruit_OLED_libraries/Adafruit_GFX.h
+++ Arduino_package/hardware/libraries/Wire/src/Adafruit_OLED_libraries/Adafruit_GFX.h
@@ -8,7 +8,6 @@
 #include "WProgram.h"
 #endif
 #include "Adafruit_OLED_libraries/gfxfont.h"
-#include <Adafruit_OLED_libraries/Adafruit_I2CDevice.h>
 
 /// A generic graphics superclass that can handle all sorts of drawing. At a
 /// minimum you can subclass and provide drawPixel(). At a maximum you can do a
