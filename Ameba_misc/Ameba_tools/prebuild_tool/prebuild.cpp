8608e63668c9eaef778763eca6a70e1d0c8a2cdb
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2024-04-09 15:22:40 +0800

    Update package structure
    - Add ameba_pro2_nn_models tool to reduce the SDK package size.
    - Update prebuild tools, platform.txt
    - Remove nn models from package

--- Ameba_misc/Ameba_tools/prebuild_tool/prebuild.cpp
+++ Ameba_misc/Ameba_tools/prebuild_tool/prebuild.cpp
@@ -12,7 +12,8 @@ g++ -o prebuild_linux prebuild.cpp -static
 strip prebuild_linux
 
 macos:
-g++ -o prebuild_macos prebuild.cpp
+g++ -arch x86_64 -arch arm64 -o prebuild_macos prebuild.cpp
+### g++ -o prebuild_macos prebuild.cpp
 ### strip prebuild_macos
 
 */
@@ -28,7 +29,8 @@ g++ -o prebuild_macos prebuild.cpp
 
 using namespace std;
 
-string common_libs_path, common_sensor_sel_libs_path, isp_camera_option;
+string common_libs_path, common_sensor_sel_libs_path, common_nn_libs_path, nn_tool_name_path;
+string isp_camera_option, nn_tool_name;
 
 int main(int argc, char *argv[]) {
     stringstream cmdss;
@@ -39,6 +41,8 @@ int main(int argc, char *argv[]) {
     chdir("../..");
 
     isp_camera_option = argv[7];
+    nn_tool_name_path = argv[8]; //"ameba_pro2_nn_models/1.0.0";
+    nn_tool_name = argv[9]; //"ameba_pro2_nn_models";
 
 #if defined(__WIN32__) // MINGW64
     string string_temp_1 = "copy ";
@@ -48,6 +52,8 @@ int main(int argc, char *argv[]) {
     common_libs_path = common_libs_path + "\\variants\\common_libs\\libarduino_sensor_sel.a";
     common_sensor_sel_libs_path = argv[6];
     common_sensor_sel_libs_path = common_sensor_sel_libs_path + "\\variants\\common_sensor_sel_libs\\";
+    common_nn_libs_path = argv[6];
+    common_nn_libs_path = common_nn_libs_path + "\\variants";
 
     cmdss.clear();
     cmdss << "if exist " << common_libs_path << "del " << common_libs_path;
@@ -62,7 +68,19 @@ int main(int argc, char *argv[]) {
     system(cmd.c_str());
 
     cmdss.clear();
-    cmdss << "if exist " << argv[5] << " rmdir /s /q  " << argv[5];
+    cmdss << "if exist " << argv[5] << " rmdir /s /q " << argv[5];
+    getline(cmdss, cmd);
+    cout << cmd << endl;
+    system(cmd.c_str());
+
+    cmdss.clear();
+    cmdss << "if exist " << nn_tool_name_path <<" xcopy /y /s \"" << nn_tool_name_path << "\" \"" << common_nn_libs_path << "\"";
+    getline(cmdss, cmd);
+    cout << cmd << endl;
+    system(cmd.c_str());
+
+    cmdss.clear();
+    cmdss << "if exist " << nn_tool_name << " rmdir /s /q " << nn_tool_name;
     getline(cmdss, cmd);
     cout << cmd << endl;
     system(cmd.c_str());
@@ -75,16 +93,18 @@ int main(int argc, char *argv[]) {
     common_libs_path = common_libs_path + "/variants/common_libs/libarduino_sensor_sel.a";
     common_sensor_sel_libs_path = argv[6];
     common_sensor_sel_libs_path = common_sensor_sel_libs_path + "/variants/common_sensor_sel_libs/";
+    common_nn_libs_path = argv[6];
+    common_nn_libs_path = common_nn_libs_path + "/variants";
 
     cmdss.clear();
     cmdss << "rm -f " << common_libs_path;
     getline(cmdss, cmd);
-    //cout << cmd << endl;
+    cout << cmd << endl;
     system(cmd.c_str());
 
-    cmdss.clear();
 //    cmdss << "cp -r ./" << argv[3] << "/* " << argv[2] << " 2> /dev/null " ;
 //    cmdss << "find ./ -mindepth 1 -maxdepth 1 -type d -name \"" << argv[3] << "\" | xargs -i cp -r {}/*" << " ./"<< argv[2];
+    cmdss.clear();
     cmdss << "find ./" << argv[3] << "/ -mindepth 1 -maxdepth 1 -type d -name \"*\" 2>/dev/null | xargs -i cp -r {}" << " ./"<< argv[2] << "/";
     getline(cmdss, cmd);
     cout << cmd << endl;
@@ -92,6 +112,18 @@ int main(int argc, char *argv[]) {
 
     cmdss.clear();
     cmdss << "rm -rf " << argv[5];
+    getline(cmdss, cmd);
+    cout << cmd << endl;
+    system(cmd.c_str());
+
+    cmdss.clear();
+    cmdss << "find ./" << nn_tool_name_path << "/ -mindepth 1 -maxdepth 1 -type d -name \"*\" 2>/dev/null | xargs -i cp -r {} ./"<< common_nn_libs_path << "/";
+    getline(cmdss, cmd);
+    cout << cmd << endl;
+    system(cmd.c_str());
+
+    cmdss.clear();
+    cmdss << "rm -rf " << nn_tool_name;
     getline(cmdss, cmd);
     cout << cmd << endl;
     system(cmd.c_str());
