ccfb6db0afa359d18a2c3d1be3a10196dfe64458
pammyleong <pammyleong96@gmail.com>
2024-05-17 15:06:20 +0800

    Update WMath.cpp
    - Update WMath.cpp to prevent hard fault during calibration

--- Arduino_package/hardware/cores/ambpro2/WMath.cpp
+++ Arduino_package/hardware/cores/ambpro2/WMath.cpp
@@ -72,7 +72,11 @@ extern long random(long howsmall, long howbig)
 
 extern long map(long x, long in_min, long in_max, long out_min, long out_max)
 {
-    return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+    if (in_max <= in_min) {
+        return x;
+    } else {
+        return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+    }
 }
 
 extern uint16_t makeWord(uint16_t w)
