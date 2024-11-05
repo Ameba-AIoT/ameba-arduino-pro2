2753a2a4e26ce1b4de457b529df09b35d91c9d04
TristonKF <yuenkaifai20@gmail.com>
2024-11-05 16:08:42 +0800

    update #277
    -Change file location for sample
      mp3 to
      Ameba_misc\Example_Samples
      folder.
    
    -update docs links in
      SDCardPlayMP3.ino and
      UVCDObjectDetectionLoop.ino.

--- Arduino_package/hardware/libraries/Multimedia/examples/SDCardPlayMP3/SDCardPlayMP3.ino
+++ Arduino_package/hardware/libraries/Multimedia/examples/SDCardPlayMP3/SDCardPlayMP3.ino
@@ -1,12 +1,12 @@
 /*  This example decodes an MP3 file from an SD card
     and plays audio through a TRRS 3.5mm audio jack module.
 
- Example guide: https://ameba-doc-arduino-sdk.readthedocs-hosted.com/en/latest/amebapro2/Example_Guides/File%20System/Play%20MP3%20with%20SD%20card.html
+ Example guide: https://ameba-doc-arduino-sdk.readthedocs-hosted.com/en/latest/amebapro2/Example_Guides/Multimedia/Play%20MP3%20with%20SD%20card.html
 
 */
 #include "AmebaFatFS.h"
 
-#define FILENAME "audio"
+#define FILENAME "Audio_test"
 #define INTERVAL 1000
 
 AmebaFatFS fs;
