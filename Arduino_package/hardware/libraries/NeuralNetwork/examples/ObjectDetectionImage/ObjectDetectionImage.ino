79075dd1b34359a5a0007d90a5c9f3591e8cdd39
Kevin Loo Keng Leong <kevinloo0326@gmail.com>
2025-02-07 15:35:39 +0800

    Update coding style

--- Arduino_package/hardware/libraries/NeuralNetwork/examples/ObjectDetectionImage/ObjectDetectionImage.ino
+++ Arduino_package/hardware/libraries/NeuralNetwork/examples/ObjectDetectionImage/ObjectDetectionImage.ino
@@ -9,14 +9,13 @@ Example guide: TBD
 
 NNObjectDetectionImage ObjDetImg;
 
-char filelist_name[] = "image_list.txt"; // modify according to image name list
+char filelist_name[] = "image_list.txt";    // modify according to image name list
 
 void setup()
 {
     Serial.begin(115200);
     ObjDetImg.modelSelect(OBJECT_DETECTION, DEFAULT_YOLOV4TINY, NA_MODEL, NA_MODEL);
     ObjDetImg.begin(filelist_name);
-
 }
 
 void loop()
