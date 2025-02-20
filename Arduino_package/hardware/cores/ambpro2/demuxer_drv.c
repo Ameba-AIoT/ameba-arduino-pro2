606aadaf4f5d3bed06ef350f7aff50d7e5d977a2
pammyleong <pammyleong96@gmail.com>
2025-02-20 09:47:56 +0800

    Modify coding style format for demuxer_drv.c,.h
    Modify coding style format for demuxer_drv.c and demuxer_drv.h

--- Arduino_package/hardware/cores/ambpro2/demuxer_drv.c
+++ Arduino_package/hardware/cores/ambpro2/demuxer_drv.c
@@ -3,14 +3,13 @@
 #include "module_demuxer.h"
 
 static demuxer_params_t demuxer_params = {
-	.start_time     = 0,
-	.stream_type    = 0,
-	.loop_mode      = 0, //0 no loop, 1 loop
+    .start_time = 0,
+    .stream_type = 0,
+    .loop_mode = 0,    // 0 no loop, 1 loop
 
-	.record_file_name = "",
-	.mem_total_size = 0,
-	.mem_block_size = 0
-};
+    .record_file_name = "",
+    .mem_total_size = 0,
+    .mem_block_size = 0};
 
 mm_context_t *demuxerInit(void)
 {
@@ -22,11 +21,11 @@ mm_context_t *demuxerDeinit(mm_context_t *p)
     return mm_module_close(p);
 }
 
-void demuxerOpen(mm_context_t *p, const char* file_name, uint8_t stream_type, uint32_t loop_mode, uint32_t start_time)
+void demuxerOpen(mm_context_t *p, const char *file_name, uint8_t stream_type, uint32_t loop_mode, uint32_t start_time)
 {
-     demuxer_params.start_time = start_time;
-     demuxer_params.stream_type = stream_type;
-     demuxer_params.loop_mode = loop_mode;
+    demuxer_params.start_time = start_time;
+    demuxer_params.stream_type = stream_type;
+    demuxer_params.loop_mode = loop_mode;
 
     // Copy the file name into the structure
     strncpy(demuxer_params.record_file_name, file_name, sizeof(demuxer_params.record_file_name) - 1);
@@ -61,4 +60,3 @@ void demuxerResume(mm_context_t *p)
     printf("Set RESUME for the MP4\r\n");
     mm_module_ctrl(p, CMD_DEMUXER_STREAM_RESUME, 0);
 }
-
--- Arduino_package/hardware/cores/ambpro2/demuxer_drv.h
+++ Arduino_package/hardware/cores/ambpro2/demuxer_drv.h
@@ -5,7 +5,7 @@
 
 mm_context_t *demuxerInit(void);
 mm_context_t *demuxerDeinit(mm_context_t *p);
-void demuxerOpen(mm_context_t *p, const char* file_name, uint8_t stream_type, uint32_t loop_mode, uint32_t start_time);
+void demuxerOpen(mm_context_t *p, const char *file_name, uint8_t stream_type, uint32_t loop_mode, uint32_t start_time);
 void demuxerStart(mm_context_t *p);
 void demuxerPause(mm_context_t *p);
 void demuxerResume(mm_context_t *p);
