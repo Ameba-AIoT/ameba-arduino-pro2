b72e0d896795fe20a0ecd0429c7cdb9a8630b5f9
Kevin Loo Keng Leong <95334490+kev0077@users.noreply.github.com>
2025-04-07 14:35:36 +0800

    Update examples (#316)
    * Update example
    
    - Simple HTTP Server ReceiveData example update
    - ObjectDetectionImage guide link update
    
    * Coding style update

--- Arduino_package/hardware/libraries/WiFi/examples/SimpleHttpWeb/ReceiveData/ReceiveData.ino
+++ Arduino_package/hardware/libraries/WiFi/examples/SimpleHttpWeb/ReceiveData/ReceiveData.ino
@@ -1,7 +1,7 @@
 /*
 
  Example guide:
- https://www.amebaiot.com/en/amebapro2-arduino-web-server-status/
+ https://ameba-doc-arduino-sdk.readthedocs-hosted.com/en/latest/ameba_pro2/amb82-mini/Example_Guides/WiFi/Simple%20Http%20Server%20to%20Receive%20Data.html
  */
 
 #include <WiFi.h>
@@ -43,8 +43,9 @@ void loop()
     WiFiClient client = server.available();
     if (client) {
         // an http request ends with a blank line
-        boolean currentLineIsBlank = true;
         Serial.println("new client");
+        String currentLine = "";
+        Serial.println("--- Start of Client Request ---");    // Added marker
         while (client.connected()) {
             if (client.available()) {
                 char c = client.read();
@@ -52,36 +53,36 @@ void loop()
                 // if you've gotten to the end of the line (received a newline
                 // character) and the line is blank, the http request has ended,
                 // so you can send a reply
-                if (c == '\n' && currentLineIsBlank) {
-                    // send a standard http response header
-                    client.println("HTTP/1.1 200 OK");
-                    client.println("Content-Type: text/html");
-                    client.println("Connection: close");    // the connection will be closed after completion of the response
-                    client.println("Refresh: 5");           // refresh the page automatically every 5 sec
-                    client.println();
-                    client.println("<!DOCTYPE HTML>");
-                    client.println("<html>");
-                    // output the value of each analog input pin
-                    for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-                        int sensorReading = analogRead(analogPins[analogChannel]);
-                        client.print("analog input ");
-                        client.print(analogChannel);
-                        client.print(" is ");
-                        client.print(sensorReading);
-                        client.println("<br />");
-                    }
-                    client.println("</html>");
-                    break;
-                }
                 if (c == '\n') {
-                    // you're starting a new line
-                    currentLineIsBlank = true;
+                    // Serial.print("Line received: "); // Indicate a new line
+                    // Serial.println(currentLine);
+                    if (currentLine.length() == 0) {
+                        Serial.println("Blank line detected - Sending HTML response.");
+                        String htmlContent = "";
+                        for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+                            int sensorReading = analogRead(analogPins[analogChannel]);
+                            htmlContent += "analog input " + String(analogChannel) + " is " + String(sensorReading) + "<br />";
+                        }
+                        htmlContent += "</body></html>";
+                        String response = "HTTP/1.1 200 OK\r\n";
+                        response += "Content-Type: text/html\r\n";
+                        response += "Connection: close\r\n";
+                        response += "Content-Length: " + String(htmlContent.length()) + "\r\n";    // Calculate and set Content-Length
+                        response += "\r\n";
+                        response += htmlContent;
+
+                        client.print(response);
+                        Serial.println("--- HTML Response Sent ---");
+                        break;
+                    } else {
+                        currentLine = "";
+                    }
                 } else if (c != '\r') {
-                    // you've gotten a character on the current line
-                    currentLineIsBlank = false;
+                    currentLine += c;
                 }
             }
         }
+        Serial.println("--- End of Client Request (Connection Closed) ---");    // Added marker
         // give the web browser time to receive the data
         delay(1);
 
