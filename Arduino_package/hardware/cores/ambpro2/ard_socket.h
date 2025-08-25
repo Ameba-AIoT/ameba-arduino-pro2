c3f37f5eb8483418c47bac8fefe87ac5d5ca3313
pammyleong <pammyleong96@gmail.com>
2025-08-25 11:36:37 +0800

    Update coding style
    Update coding style

--- Arduino_package/hardware/cores/ambpro2/ard_socket.h
+++ Arduino_package/hardware/cores/ambpro2/ard_socket.h
@@ -31,6 +31,6 @@ int get_receive(int sock, uint8_t *data, int length, int flag, uint32_t *peer_ad
 int get_receive_v6(int server_fd, void *recv_data, int len, int flags, uint32_t *peer_addr, uint16_t *peer_port);
 int sendto_data(int sock, const uint8_t *data, uint32_t len, uint32_t peer_ip, uint16_t peer_port);
 int sendto_data_v6(int sock, const void *send_data, size_t len, uint32_t peer_ip, uint16_t peer_port);
-void ipv6_udp_server(char* data_recv, const char* data_send);
+void ipv6_udp_server(char *data_recv, const char *data_send);
 
 #endif
