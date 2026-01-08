0f8a4089bddc6edecb3e0103918aba5a863b55b2
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2026-01-08 17:38:58 +0800

    Update RTSP printInfo
    API Updates:
    - Update printInfo for showing IP and port at same row

--- Arduino_package/hardware/libraries/Multimedia/src/RTSP.cpp
+++ Arduino_package/hardware/libraries/Multimedia/src/RTSP.cpp
@@ -109,3 +109,10 @@ void RTSP::printInfo(void)
     int port = getPort();
     amb_ard_printf(ARD_LOG_INF, "\r\n[INFO] %d\n", port);
 }
+
+void RTSP::printInfo(char* ip)
+{
+    int port = getPort();
+    amb_ard_printf(ARD_LOG_INF, "\r\n[INFO] rtsp://%s:%d\n", ip, port);
+}
+
