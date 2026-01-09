03776e478fa34d0681e1c7c9560f7bab49bc667c
Michael Zhang <35476432+M-ichae-l@users.noreply.github.com>
2026-01-09 10:34:28 +0800

    Update all printInfo (#367)
    API Updates:
    - Update printInfo for RTSP showing IP and port at same row

--- Arduino_package/hardware/libraries/Multimedia/examples/StreamRTSP/VideoOnly/VideoOnly.ino
+++ Arduino_package/hardware/libraries/Multimedia/examples/StreamRTSP/VideoOnly/VideoOnly.ino
@@ -80,8 +80,5 @@ void printInfo(void)
     IPAddress ip = WiFi.localIP();
 
     Serial.println("- RTSP -");
-    Serial.print("rtsp://");
-    Serial.print(ip);
-    Serial.print(":");
-    rtsp.printInfo();
+    rtsp.printInfo(ip.get_address());
 }
