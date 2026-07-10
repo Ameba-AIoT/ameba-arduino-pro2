ab4f1f431c0cd7c5aa0bb26a635cc3076cc56ab7
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2026-07-10 15:24:59 +0800

    Update Adafruit_SPIDevice.h

--- Arduino_package/hardware/libraries/Wire/src/Adafruit-PWM-Servo-Driver-Library/Adafruit_BusIO/Adafruit_SPIDevice.h
+++ Arduino_package/hardware/libraries/Wire/src/Adafruit-PWM-Servo-Driver-Library/Adafruit_BusIO/Adafruit_SPIDevice.h
@@ -146,7 +146,7 @@ private:
   bool _begun;
 };
 
-#if defined(BOARD_AMB82_ZERO) || defined(BOARD_AMB82_ZERO)
+#if defined(BOARD_AMB82_MINI) || defined(BOARD_AMB82_ZERO)
 #undef BUSIO_USE_FAST_PINIO
 #endif
 
